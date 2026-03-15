fdc53f78f27564b5ee2575949e7e894372f751f1
251250254 <251250254@smail.nju.edu.cn>
2026-03-15 18:23:02 +0800

    feat:添加了插入排序

--- leetcode/array/sort/SortArray.java
+++ leetcode/array/sort/SortArray.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * 数组排序工具类，集中存放多种排序算法
  */
 public class SortArray {
-    // ========== 1. 冒泡排序（核心方法） ==========
+    // ========== 1. 冒泡排序（基础） ==========
     public void bubbleSort(int[] nums){
         if(nums==null||nums.length<=1){return;}
         int n=nums.length;
@@ -21,12 +21,30 @@ public class SortArray {
             }
         }
     }
+
+    // ========== 2. 插入排序（比冒泡高效，基础） ==========
+    public void insertionSort(int[] nums){
+        if(nums==null||nums.length<=1){return;}
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            int tmp=nums[i];
+            int j=i-1;
+            for(;j>=0;j--){
+                if(tmp<nums[j]){
+                    nums[j+1]=nums[j];
+                }
+                else{break;}
+            }
+            nums[j+1]=tmp;
+        }
+    }
+
     public  static void main(String[] args){
         SortArray sorter=new SortArray();
         int[] testNums = {5,2,3,1,4};
         System.out.println("排序前："+Arrays.toString(testNums));
 
-        sorter.bubbleSort(testNums);
+        sorter.insertionSort(testNums);
         System.out.println("排序后："+Arrays.toString(testNums));
     }
 }
